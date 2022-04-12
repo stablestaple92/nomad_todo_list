701322bbf88ad9f538fae440e43253777ecde5d8
Lim Chang-Hyun <stablestaple92@gmail.com>
2022-04-12 18:44:34 +0900

    first commit

---
+++ src/ToDoList.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+export default ToDoList;
